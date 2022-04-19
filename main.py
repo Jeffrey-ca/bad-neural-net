27ef41e6ae316ba4782e5a7ba2354e127a2f553d
chadman123 <83678590+chadman123@users.noreply.github.com>
2022-04-19 16:48:19 -0400

    put outline in

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from flask import Flask, request, abort
+from trade_options import *
+from neural_net import *
 
 app = Flask(__name__)
 
@@ -7,9 +9,18 @@ app = Flask(__name__)
 def webhook():
     if request.method == 'POST':
         print(request.json)
+        # TODO add way to read json file and turn data into array
+        rand_nums = np.zeros(shape=(25))
+        X = np.array(rand_nums)  # array from request
+        nn_output = nn(X)
+        if nn_output[0] >= .8:
+            long()
+        elif nn_output[1] >= .8:
+            short()
         return 'success', 200
     else:
         abort(400)
+
 
 if __name__ == '__main__':
     app.run()
---
+++ trade_options.py
@@ -0,0 +1,13 @@
+
+# TODO add code for this
+
+def long():
+    # send buy to trading platform
+
+
+def short1():
+    # send sell to trading platform
+
+
+def close():
+    # send close position to platform
