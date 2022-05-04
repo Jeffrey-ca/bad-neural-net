4d97177f8fa928b3e709b5503b88c4c7bee97d39
chadman123 <83678590+chadman123@users.noreply.github.com>
2022-05-03 22:49:06 -0400

    Update webhook_test.py

--- webhook_test.py
+++ webhook_test.py
@@ -1,9 +1,10 @@
 import requests
 import json
 
-webhook_url = 'http://a8ea-69-204-191-79.ngrok.io/webhook'
+webhook_url = 'http://54c2-69-204-191-79.ngrok.io/webhook'
 
-data = { 'big dick': '> 2 inches',
-         'small penis': '< 1.9 inch' }
+data = {'big dick': '> 2 inches',
+        'small penis': '< 1.9 inch'}
 
-r = requests.post(webhook_url, data=json.dumps(data), headers={'Content-type': 'application/json'})
+r = requests.post(webhook_url, data=json.dumps(
+    data), headers={'Content-type': 'application/json'})
