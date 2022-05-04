391338dab1ca6fed1a9436c65c254c4c8635a112
chadman123 <83678590+chadman123@users.noreply.github.com>
2022-05-03 21:53:10 -0400

    made a \ to a /

--- main.py
+++ main.py
@@ -5,42 +5,12 @@ from flask_ngrok import run_with_ngrok
 import json
 
 app = Flask(__name__)
-'''run_with_ngrok(app)'''
-
-
-def webhook_to_array():
-    a = 1 + 1
+run_with_ngrok(app)
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
     if request.method == 'POST':
         print(request.json)
-        return 200
-        print(output(X))
-        save_in_out(X, output(X))
-        print(request.json)
-        long = 0
-        short = 0
-        trade = long + short
-        if trade == 0:
-            # TODO add way to read json file and turn data into array
-            rand_nums = np.zeros(shape=(25))
-            X = np.array(rand_nums)  # array from request
-            nn_output = nn(X)
-            if nn_output[0] >= .8 and long != 1:
-                long()
-                long += 1
-            elif nn_output[1] >= .8 and short != 1:
-                short()
-                short += 1
-        if trade == 1:
-            nn_output = nn(X)
-            if long == 1 and nn_output[0] < .5:
-                close()
-                long -= 1
-            if short == 1 and nn_output[1] < .5:
-                close()
-                short -= 1
         return 'success', 200
     else:
         abort(400)
--- webhook_test.py
+++ webhook_test.py
@@ -1,16 +1,9 @@
 import requests
 import json
 
-webhook_url = 'https://a8a2-69-204-191-79.ngrok.io\webhook'
-data = {
-    "Yellow Diamond" : "{{plot_1}}",
-    "Trend Change" : "{{plot_2}}",
-    "Green Dot" : "{{plot_3}}",
-    "Red X" : "{{plot_4}}",
-    "Manipulation" : "{{plot_5}}",
-    "Red Diamond" : "{{plot_6}}",
-    "Blood Diamond" : "{{plot_7}}"
-}
+webhook_url = 'http://a8ea-69-204-191-79.ngrok.io/webhook'
 
-r = requests.post(webhook_url, data=json.dumps(data), headers={
-                  'Content-type': 'application/json'})
+data = { 'big dick': '> 2 inches',
+         'small penis': '< 1.9 inch' }
+
+r = requests.post(webhook_url, data=json.dumps(data), headers={'Content-type': 'application/json'})
