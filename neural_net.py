7d3fd1dc90362d1dc26484d6d76f624435e62316
chadman123 <83678590+chadman123@users.noreply.github.com>
2022-05-22 19:17:20 -0400

    fixed neural net
    @jit code broke neural net

--- neural_net.py
+++ neural_net.py
@@ -4,8 +4,7 @@ import math
 import random
 import pickle
 from timeit import default_timer as timer
-# from trade_options import *
-
+from trade_options import *
 
 
 # Loads weights, biases, and training data
@@ -28,8 +27,34 @@ pickle_in2 = open('outputs.pickle', 'rb')
 out = pickle.load(pickle_in2)
 
 
-# Two activation functions for neural net
+def reset_weights():
+    w1 = np.zeros(shape=(50, 40))
+    b1 = np.zeros(shape=(50))
+    w2 = np.zeros(shape=(50, 50))
+    b2 = np.zeros(shape=(50))
+    w3 = np.zeros(shape=(50, 1))
+    b3 = np.zeros(shape=(1))
+    PO_w1 = open("w1.pickle", "wb")
+    pickle.dump(w1, PO_w1)
+    PO_w1.close()
+    PO_b1 = open("b1.pickle", "wb")
+    pickle.dump(b1, PO_b1)
+    PO_b1.close()
+    PO_w2 = open("w2.pickle", "wb")
+    pickle.dump(w2, PO_w2)
+    PO_w2.close()
+    PO_b2 = open("b2.pickle", "wb")
+    pickle.dump(b2, PO_b2)
+    PO_b2.close()
+    PO_w3 = open("w3.pickle", "wb")
+    pickle.dump(w3, PO_w3)
+    PO_w3.close()
+    PO_b3 = open("b3.pickle", "wb")
+    pickle.dump(b3, PO_b3)
+    PO_b3.close()
 
+
+# Two activation functions for neural net
 
 def activation2(output):
     for n in range(0, 100):
@@ -43,19 +68,19 @@ def activation3(output):
 
 # TODO make nn function parallel
 # Calculates loss of the neural net
-'''def nn():
-    loss = np.zeros(1)
+def nn(a, b):
+    loss = 0
     for i in range(len(inp.keys())):
-        X = inp[i]
-        out[i] =  np.dot(out[i], np.array(-1))
-        to = out[i]
-        loss_array2 = abs(loss_math(X, to))
-        loss_2x = np.square(loss_array2)
-        loss = np.add(loss, loss_2x)
-    return array_to_float(loss)
+        o3 = loss_math(inp[i])
+        loss_array = abs(np.subtract(o3, out[i]))
+        loss_2x = np.square(loss_array)
+        for p in range(len(loss_2x)):
+            loss = abs(loss) + abs(loss_2x[p])
+    return np.sum(loss)
+                                    
 
-@jit(nopython=True, parallel=True)
-def loss_math(X, to):
+
+def loss_math(X):
     o1 = np.add(np.dot(w1, X), b1)
     for n in range(0, 100):
         o1[n] = (2/(1+(math.e**-(2*o1[n]))))-1
@@ -63,19 +88,10 @@ def loss_math(X, to):
     for a in range(0, 100):
         o2[a] = 1/(1+(math.e**-(o2[a])))
     o3 = np.add(np.dot(o2, w3), b3)
-    return np.add(o3, to)
+    return o3
 
 
-def array_to_float(num):
-    string = str(num)
-    a = list(string)
-    a.pop(0)
-    a.pop(-1)
-    a = ''.join(a)
-    return float(num)'''
-
-
-def nn():
+'''def nn(x, to):
     loss = 0
     for i in range(len(inp.keys())):
         o1 = np.add(np.dot(w1, inp[i]), b1)
@@ -86,10 +102,12 @@ def nn():
         loss_array = abs(np.subtract(o3, out[i]))
         loss_2x = np.square(loss_array)
         for p in range(len(loss_2x)):
-            loss = loss + loss_2x[p]
-    return np.sum(loss)
+            loss = abs(loss) + abs(loss_2x[p])
+    return np.sum(loss)'''
+
 
 # Produces the output of neural net
+
 
 @jit(nopython=True, parallel=True)
 def output(X):
@@ -98,65 +116,62 @@ def output(X):
         o1[n] = (2/(1+(math.e**-(2*o1[n]))))-1
     o2 = np.add(np.dot(w2, o1), b2)
     for a in range(0, 100):
-        o2[a] = 1/(1+(math.e**-(o2[n])))
+        o2[a] = 1/(1+(math.e**-(o2[a])))
     o3 = np.add(np.dot(o2, w3), b3)
     return o3
 
 
 # Train the neural net
 
-
-def train(num1):
+@jit(nopython=True, parallel=True)
+def train(num1, X, to):
     for x in range(0, num1, 1):
-        for z in range(0, 1, 1):
-            for a in range(1, 2, 1):
-                for c in range(0, 100):
-                    for b in range(0, 40):
-                        hold_loss_w1 = nn()
-                        hold_w1 = w1[c][b]
-                        w1[c][b] = random.uniform(-1.0, 1.0)
-                        loss_test_w1 = nn()
-                        if abs(hold_loss_w1) < abs(loss_test_w1):
-                            w1[c][b] = hold_w1
-                for d in range(0, 100):
-                    hold_loss_b1 = nn()
-                    hold_b1 = b1[d]
-                    b1[d] = random.uniform(-1.0, 1.0)
-                    loss_test_b1 = nn()
-                    if abs(hold_loss_b1) < abs(loss_test_b1):
-                        b1[d] = hold_b1
-                for e in range(0, 100):
-                    for f in range(0, 100):
-                        hold_loss_w2 = nn()
-                        hold_w2 = w2[e][f]
-                        w2[e][f] = random.uniform(-1.0, 1.0)
-                        loss_test_w2 = nn()
-                        if abs(hold_loss_w2) < abs(loss_test_w2):
-                            w2[e][f] = hold_w2
-                for g in range(0, 100):
-                    hold_loss_b2 = nn()
-                    hold_b2 = b2[g]
-                    b2[g] = random.uniform(-1.0, 1.0)
-                    loss_test_b2 = nn()
-                    if abs(hold_loss_b2) < abs(loss_test_b2):
-                        b2[g] = hold_b2
-                for h in range(0, 100, 1):
-                    for i in range(0, 1, 1):
-                        hold_loss_w3 = nn()
-                        hold_w3 = w3[h][i]
-                        w3[h][i] = random.uniform(-1.0, 1.0)
-                        loss_test_w3 = nn()
-                        if abs(hold_loss_w3) < abs(loss_test_w3):
-                            w3[h][i] = hold_w3
-                for j in range(0, 1):
-                    hold_loss_b3 = nn()
-                    hold_b3 = b3[j]
-                    b3[j] = random.uniform(-1.0, 1.0)
-                    loss_test_b3 = nn()
-                    if abs(hold_loss_b3) < abs(loss_test_b3):
-                        b3[j] = hold_b3
+        for c in range(0, 100):
+            for b in range(0, 40):
+                hold_loss_w1 = nn(X, to)
+                hold_w1 = w1[c][b]
+                w1[c][b] = random.uniform(-1.0, 1.0)
+                loss_test_w1 = nn(X, to)
+                if abs(hold_loss_w1) < abs(loss_test_w1):
+                    w1[c][b] = hold_w1
+        for d in range(0, 100):
+            hold_loss_b1 = nn(X, to)
+            hold_b1 = b1[d]
+            b1[d] = random.uniform(-1.0, 1.0)
+            loss_test_b1 = nn(X, to)
+            if abs(hold_loss_b1) < abs(loss_test_b1):
+                b1[d] = hold_b1
+        for e in range(0, 100):
+            for f in range(0, 100):
+                hold_loss_w2 = nn(X, to)
+                hold_w2 = w2[e][f]
+                w2[e][f] = random.uniform(-1.0, 1.0)
+                loss_test_w2 = nn(X, to)
+                if abs(hold_loss_w2) < abs(loss_test_w2):
+                    w2[e][f] = hold_w2
+        for g in range(0, 100):
+            hold_loss_b2 = nn(X, to)
+            hold_b2 = b2[g]
+            b2[g] = random.uniform(-1.0, 1.0)
+            loss_test_b2 = nn(X, to)
+            if abs(hold_loss_b2) < abs(loss_test_b2):
+                b2[g] = hold_b2
+        for h in range(0, 100, 1):
+            for i in range(0, 1, 1):
+                hold_loss_w3 = nn(X, to)
+                hold_w3 = w3[h][i]
+                w3[h][i] = random.uniform(-1.0, 1.0)
+                loss_test_w3 = nn(X, to)
+                if abs(hold_loss_w3) < abs(loss_test_w3):
+                    w3[h][i] = hold_w3
+        for j in range(0, 1):
+            hold_loss_b3 = nn(X, to)
+            hold_b3 = b3[j]
+            b3[j] = random.uniform(-1.0, 1.0)
+            loss_test_b3 = nn(X, to)
+            if abs(hold_loss_b3) < abs(loss_test_b3):
+                b3[j] = hold_b3
     save_weights()
-
 
 
 def save_weights():
@@ -327,3 +342,4 @@ def auto_label_last():
     pickle_out2 = open('outputs.pickle', 'wb')
     pickle.dump(out, pickle_out2)
     pickle_out2.close()
+
--- tools.py
+++ tools.py
@@ -5,9 +5,9 @@ from neural_net import *
 start = timer()
 for b in range(1):
     for i in range(len(inp.keys())):
-        train(1)
-        a = nn()
-        print(a)
+        train(1, inp[i], out[i])
+        save_weights()
+        print(nn(inp[i], out[i]))
 for i in range(len(inp.keys())):
     print(i, output(inp[i]))
     print(i, out[i])
@@ -25,15 +25,14 @@ pickle_out6 = open('X.pickle', 'wb')
 pickle.dump(X, pickle_out6)
 pickle_out6.close()
 inp = {}
-out = {}
-pickle_out = open('inputs.pickle', 'wb')
+out = {}'''
+'''pickle_out = open('inputs.pickle', 'wb')
 pickle.dump(inp, pickle_out)
 pickle_out.close()
 pickle_out2 = open('outputs.pickle', 'wb')
 pickle.dump(out, pickle_out2)
-pickle_out2.close()'''
-
-
+pickle_out2.close()
+print(out)'''
 # reset neural net weights and biases
 '''w1 = np.zeros(shape=(100, 40))
 b1 = np.zeros(shape=(100))
@@ -58,69 +57,4 @@ pickle.dump(w3, PO_w3)
 PO_w3.close()
 PO_b3 = open("b3.pickle", "wb")
 pickle.dump(b3, PO_b3)
-PO_b3.close()
-'''
-'''def nn(in_len):
-    loss = 0
-    for i in range(in_len):
-        o1 = np.add(np.dot(w1, inp[i]), b1)
-        activation3(o1)
-        o2 = np.add(np.dot(w2, o1), b2)
-        activation2(o2)
-        o3 = np.add(np.dot(o2, w3), b3)
-        loss_array = abs(np.subtract(o3, out[i]))
-        loss_2x = np.square(loss_array)
-        for p in range(len(loss_2x)):
-            loss = loss + loss_2x[p]
-    return np.sum(loss)
-
-
-
-
-PO_b3 = open("loss.pickle", "wb")
-pickle.dump(loss, PO_b3)
-PO_b3.close()
-
-
-PI_w1 = open('loss.pickle', 'rb')
-loss = pickle.load(PI_w1)'''
-
-
-'''def nn():
-    loss = 0
-    for i in range(len(inp.keys())):
-        a = len(inp.keys())
-        X = inp[i]
-        to = out[i]
-        loss += nn(X, to)
-    return loss
-
-@jit(nopython=True, parallel=True)
-def loss_math(X, to):
-    o1 = np.add(np.dot(w1, X), b1)
-    activation3(o1)
-    o2 = np.add(np.dot(w2, o1), b2)
-    activation2(o2)
-    o3 = np.add(np.dot(o2, w3), b3)
-    loss_array = abs(np.subtract(o3, to))
-    loss_2x = np.square(loss_array)
-    for p in range(len(loss_2x)):
-        loss = loss + loss_2x[p]
-    return np.sum(loss)
-
-'''
-'''def nn():
-    loss = 0
-    for i in range(len(inp.keys())):
-        o1 = np.add(np.dot(w1, inp[i]), b1)
-        activation3(o1)
-        o2 = np.add(np.dot(w2, o1), b2)
-        activation2(o2)
-        o3 = np.add(np.dot(o2, w3), b3)
-        loss_array = abs(np.subtract(o3, out[i]))
-        loss_2x = np.square(loss_array)
-        for p in range(len(loss_2x)):
-            loss = loss + loss_2x[p]
-    return np.sum(loss)'''
-
-
+PO_b3.close()'''
